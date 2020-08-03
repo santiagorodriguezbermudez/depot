3079b1d74cc865f51fabe541abe7a2fbc40637bf
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-03 17:58:14 -0500

    Implemented mailers for shipping

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -13,7 +13,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   test "requires item in cart" do
     get new_order_url
     assert_redirected_to store_index_path
-    assert_equal flass[:notice], 'Your cart is empty'
+    assert_equal flash[:notice], 'Your cart is empty'
   end
 
   test "should get new" do
