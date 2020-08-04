8a4350f95288ea1ebbcbd7b24a317941c56f6cb3
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-04 07:18:41 -0500

    Added active job for processing fake order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,9 @@ class OrdersController < ApplicationController
     if order_params[:pay_type] == 'Credit card'
       params.require(:order).permit(:credit_card_number, :expiration_date)
     elsif order_params[:pay_type] == 'Check'
-      params.requite(:order).permit(:routing_number, :account_number)
+      params.require(:order).permit(:routing_number, :account_number)
     elsif order_params[:pay_type] == 'Purchase Order'
-      params.requite(:order).permit(:po_number)
+      params.require(:order).permit(:po_number)
     else
       {}
     end
@@ -47,7 +47,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
