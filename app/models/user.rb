27008708c89e335b1bd79daaea262d04eac1090e
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-06 07:28:14 -0500

    Added l18n to cart and store for spanish translation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_secure_password
-  after_destoy :ensure_an_admin_remains
+  after_destroy :ensure_an_admin_remains
 
   class Error < StandardError
   end
